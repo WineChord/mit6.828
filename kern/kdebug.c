02ed480e19133ef92430cb2b83ebdadac8aa9104
Qizhou Guo <guoqizhou19@gmail.com>
2021-02-15 11:44:18 +0800

    check usd,stab,stabstr in debuginfo_eip

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -142,6 +142,9 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 		// Make sure this memory is valid.
 		// Return -1 if it is not.  Hint: Call user_mem_check.
 		// LAB 3: Your code here.
+		int r = user_mem_check(curenv, (void *)usd, sizeof(struct UserStabData), PTE_U);
+		if (r < 0)
+			return -1;
 
 		stabs = usd->stabs;
 		stab_end = usd->stab_end;
@@ -150,6 +153,12 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 
 		// Make sure the STABS and string table memory is valid.
 		// LAB 3: Your code here.
+		r = user_mem_check(curenv, (void *)stabs, sizeof(stabs), PTE_U);
+		if (r < 0)
+			return -1;
+		r = user_mem_check(curenv, (void *)stabstr, sizeof(stabstr), PTE_U);
+		if (r < 0)
+			return -1;
 	}
 
 	// String table validity checks
