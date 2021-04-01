0d727b2de6a4d608b82ecd14ac16887a913384e5
Qizhou Guo <guoqizhou19@gmail.com>
2021-04-01 18:44:43 +0800

    lab5 ex7

--- kern/syscall.c
+++ kern/syscall.c
@@ -138,7 +138,18 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+	// panic("sys_env_set_trapframe not implemented");
+	struct Env *e;
+	int r = envid2env(envid, &e, 1);
+	if(r < 0)
+		return r;
+	user_mem_assert(e, tf, sizeof(*tf), PTE_W);
+	e->env_tf = *tf;
+	e->env_tf.tf_eflags |= FL_IF;
+	e->env_tf.tf_eflags &= ~FL_IOPL_3;
+	e->env_tf.tf_cs |= 0x3;
+	e->env_tf.tf_ss |= 0x3;
+	return 0; 
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -440,6 +451,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_exofork();
 	case SYS_env_set_status:
 		return sys_env_set_status(a1, a2);
+	case SYS_env_set_trapframe:
+		return sys_env_set_trapframe(a1, (struct Trapframe *)a2);
 	case SYS_env_set_pgfault_upcall:
 		return sys_env_set_pgfault_upcall(a1, (void *)a2);
 	case SYS_page_alloc:
