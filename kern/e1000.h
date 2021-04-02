e346406a1608a73aa5a98c749299d21bd6b87872
Qizhou Guo <guoqizhou19@gmail.com>
2021-04-02 20:28:27 +0800

    lab6 ex5

--- kern/e1000.h
+++ kern/e1000.h
@@ -1,11 +1,55 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
-#include "pmap.h" // mmio_map_region
+#include <kern/pmap.h> // mmio_map_region
+#include <inc/error.h>
 
 #define E1000_DEV_ID_82540EM             0x100E
 #define E1000_VEND_ID_82540EM            0x8086
 
-volatile uint32_t *e1000va;
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_CT_ETH 0x00000100    /* Ethernet is 10h */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+#define E1000_TCTL_COLD_F 0x00040000    /* for full duplex */
+
+
+struct tx_desc
+{
+    uint64_t addr;
+    uint16_t length;
+    uint8_t cso;
+    uint8_t cmd;
+    uint8_t status;
+    uint8_t css;
+    uint16_t special;
+}__attribute__((packed));
+
+
+#define NTXDESC 32 // # of transmit descriptors
+#define TXDESCSIZE (NTXDESC*sizeof(struct tx_desc))
+#define TXBUFLEN 1520 // the maximum size of an Ethernet packet is 1518 bytes
+#define TXBUFBASE (NTXDESC*sizeof(struct tx_desc))
+
+volatile uint32_t *e1000va; // virtual address for E1000 mmio region
+
+struct tx_buf
+{
+    char buf[TXBUFLEN];
+}tx_bufs[NTXDESC];
+
+
+struct tx_desc *tx_descs; 
+
 
 #endif  // SOL >= 6
--- kern/pci.c
+++ kern/pci.c
@@ -257,7 +257,46 @@ pci_init_attach(struct pci_func *f)
 	// To test your mapping, try printing out the device status register (section 13.4.2). 
 	// This is a 4 byte register that starts at byte 8 of the register space. 
 	// You should get 0x80080783, which indicates a full duplex link is up at 1000 MB/s, among other things.
-	cprintf("device status register: %08x\n", e1000va[8/4]);
+	cprintf("device status register: %08x\n", e1000va[E1000_STATUS/4]);
+	// start transmit initialization
+	// 1. allocate a region of memory for the transmit descriptor list
+	struct PageInfo *p = page_alloc(ALLOC_ZERO);
+	if(p == NULL)
+		return -E_NO_MEM;
+	physaddr_t pa = page2pa(p);
+	tx_descs = (struct tx_desc *)(page2kva(p)); 
+	// 2. decide package buffer addresses and make transmit descriptors refer to them 
+	for(int i = 0; i < NTXDESC; i++) {
+		tx_descs[i].addr = PADDR(&tx_bufs[i]);
+	}
+	// 3. Program the Transmit Descriptor Base Address 
+	// (TDBAL/TDBAH) register(s) with the address of the region. 
+	// TDBAL is used for 32-bit addresses
+	// and both TDBAL and TDBAH are used for 64-bit addresses
+	// TDBAL is at offset 03800 (see table 13.2 page 219)
+	e1000va[E1000_TDBAL/4] = pa; // E1000 interact directly with physical address
+	// 4. Set the Transmit Descriptor Length (TDLEN) register to the size (in bytes) of the descriptor ring.
+	// This register must be 128-byte aligned.
+	e1000va[E1000_TDLEN/4] = TXDESCSIZE;
+	// 5. The Transmit Descriptor Head and Tail (TDH/TDT) registers are initialized (by hardware) to 0b
+	// after a power-on or a software initiated Ethernet controller reset. Software should write 0b to both
+	// these registers to ensure this.
+	e1000va[E1000_TDH] = 0;
+	e1000va[E1000_TDT] = 0; 
+	// 6. Initialize the Transmit Control Register (TCTL) for desired operation to include the following:
+	// • Set the Enable (TCTL.EN) bit to 1b for normal operation.
+	// • Set the Pad Short Packets (TCTL.PSP) bit to 1b.
+	// • Configure the Collision Threshold (TCTL.CT) to the desired value. Ethernet standard is 10h.
+	// This setting only has meaning in half duplex mode.
+	// • Configure the Collision Distance (TCTL.COLD) to its expected value. For full duplex
+	// operation, this value should be set to 40h. For gigabit half duplex, this value should be set to
+	// 200h. For 10/100 half duplex, this value should be set to 40h.
+	e1000va[E1000_TCTL] = E1000_TCTL_EN|E1000_TCTL_PSP|E1000_TCTL_CT_ETH;
+	e1000va[E1000_TCTL] |= E1000_TCTL_COLD_F; // assume full duplex
+	// 7. For TIPG, refer to the default values described in table 13-77 of 
+	// section 13.4.34 for the IEEE 802.3 standard IPG (don't use the values in the table in section 14.5).
+	// IPGR2: 6   IPGR1: 4    IPGT: 10
+	e1000va[E1000_TIPG] = 10; 
 	return 0;
 }
 
