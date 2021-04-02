39957e3067c03568b28ea30e55d924267722db5b
Qizhou Guo <guoqizhou19@gmail.com>
2021-04-02 23:25:24 +0800

    lab6 ex8

--- net/output.c
+++ net/output.c
@@ -10,4 +10,13 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+	envid_t env;
+	int perm, r;
+	for(;;) {
+		r = ipc_recv(&env, &nsipcbuf, &perm);
+		if(r != 0) continue;
+		r = sys_net_tx(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len);
+		if(r == -E_TXQUEUE_FULL)
+			cprintf("tx queue full\n");
+	}
 }
